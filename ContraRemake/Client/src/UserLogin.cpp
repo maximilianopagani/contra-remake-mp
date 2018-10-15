ef3950ca7096b20e7e9c1239a3217e3635895c72
Ignacio Chiappe <nacho.chiappe@gmail.com>
2018-10-14 22:49:48 -0300

    Corrijo el lo del path de las texturas para que estén todas en la raíz.

---
+++ ContraRemake/Client/src/UserLogin.cpp
@@ -0,0 +1,12 @@
+/*
+ * UserLogin.cpp
+ *
+ *  Created on: 13 oct. 2018
+ *      Author: nacho
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <iostream>
+
---
+++ ContraRemake/Client/src/UserLogin.hh
@@ -0,0 +1,13 @@
+/*
+ * UserLogin.hh
+ *
+ *  Created on: 13 oct. 2018
+ *      Author: nacho
+ */
+
+#ifndef CLIENT_SRC_USERLOGIN_HH_
+#define CLIENT_SRC_USERLOGIN_HH_
+
+
+
+#endif /* CLIENT_SRC_USERLOGIN_HH_ */
